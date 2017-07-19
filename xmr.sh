0ec6343bdd8b0baf7d6733be6b426bcb6c3dc343
nviettien191 <n.viettien191655320@gmail.com>
2017-07-19 10:35:00 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update && sudo apt-get upgrade -y 
 apt-get install -y git make 
-apt-get install gcc g++ 
+apt-get install -y gcc g++ 
 apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake
 sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 1))
 git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && chmod +x build.sh && ./build.sh
