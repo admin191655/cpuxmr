186302e33ced8baa6962e0a8054f717c16da606e
nviettien191 <n.viettien191655320@gmail.com>
2017-07-19 10:17:01 +0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get update && sudo apt-get upgrade -y 
+apt-get install -y git make 
+apt-get install gcc g++ 
+apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake
+sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 1))
+git clone https://github.com/JayDDee/cpuminer-opt.git && cd cpuminer-opt && chmod +x build.sh && ./build.sh
+./autogen.sh
+CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl
+make
+./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u nhac.tien1988@gmail.com -p x 
