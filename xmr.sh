d7cceda750022bdd9eb70eb44b3d770a25ae7d74
nviettien191 <n.viettien191655320@gmail.com>
2017-08-22 20:27:56 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -3,6 +3,6 @@ apt-get update && sudo apt-get upgrade -y
 apt-get install -y git make 
 apt-get install -y gcc g++ 
 apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake
-sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 16))
+sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 8))
 git clone https://github.com/JayDDee/cpuminer-opt && cd cpuminer-opt && chmod +x build.sh && ./build.sh || true && sudo make install
 ./cpuminer -a cryptonight -o stratum+tcp://us-east.cryptonight-hub.miningpoolhub.com:20580 -u nvt191655320.tien -p x
